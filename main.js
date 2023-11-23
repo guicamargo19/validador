7f5318bfa0ed0ac8fa5f6b5a83c5da63540d9038
camargo <gui.ferreira.camargo@gmail.com>
2023-11-22 21:49:04 -0300

    Initial commit - Exercício HTML/JS

--- main.js
+++ main.js
@@ -5,21 +5,42 @@ const campoA = document.getElementById('campoA');
 let validForm = false;
 
 function validaCampoB(campoA, campoB) {
-    const CampoBMaiorQueCampoA = campoB > campoA;
-    return CampoBMaiorQueCampoA;
+    campoA = parseFloat(campoA);
+    campoB = parseFloat(campoB);
+    return campoB > campoA;
 }
 
 campoB.addEventListener('input', function(event) {
     console.log(event.target.value);
     validForm = validaCampoB(campoA.value, event.target.value);
 
-    if (!validForm) {
+    if (!validForm && campoA.value != '') {
         campoB.classList.add('invalid');
         document.querySelector('.invalid-message').style.display = 'block';
         document.querySelector('.valid-message').style.display = 'none';
         campoB.classList.remove('valid');
         document.querySelector('.success-message').style.display = 'none';
-    } else {
+    } else if (campoA.value != ''){
+        campoB.classList.add('valid');
+        document.querySelector('.valid-message').style.display = 'block';
+        document.querySelector('.invalid-message').style.display = 'none';
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
+
+campoA.addEventListener('input', function(event) {
+    console.log(event.target.value);
+    if (campoB.value != '') {
+        validForm = validaCampoB(event.target.value, campoB.value);
+    }
+
+    if (!validForm && campoB.value != '') {
+        campoB.classList.add('invalid');
+        document.querySelector('.invalid-message').style.display = 'block';
+        document.querySelector('.valid-message').style.display = 'none';
+        campoB.classList.remove('valid');
+        document.querySelector('.success-message').style.display = 'none';
+    } else if (campoB.value != ''){
         campoB.classList.add('valid');
         document.querySelector('.valid-message').style.display = 'block';
         document.querySelector('.invalid-message').style.display = 'none';
@@ -31,7 +52,7 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
 
     const mensagemSucesso = `Número do campo B: <b>${campoB.value}</b> é maior que o número do campo A: <b>${campoA.value}`;
-    const mensagemErro = `Número do campo B: <b>${campoB.value}</b> <b>NÃO</b> é maior que o número do campo A: <b>${campoA.value}`;
+    const mensagemErro = `Número do campo B: <b>${campoB.value}</b>, <b>NÃO</b> é maior que o número do campo A: <b>${campoA.value}`;
 
     validForm = validaCampoB(campoA.value, campoB.value)
     
