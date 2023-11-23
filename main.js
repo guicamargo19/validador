6dd9c9f97c27b848df054175e150d0732e9c615b
camargo <gui.ferreira.camargo@gmail.com>
2023-11-22 21:06:20 -0300

    Initial commit - Exercício HTML/JS

--- main.js
+++ main.js
@@ -1,46 +1,62 @@
-const form = document.getElementById('form-deposito');
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
-let formValido = false;
+const form = document.getElementById('form');
+const campoB = document.getElementById('campoB');
+const campoA = document.getElementById('campoA');
 
-function validaNome(nomeCompleto) {
-    const nomeArray = nomeCompleto.split(' ');
-    return nomeArray.length >= 2;
+let validForm = false;
+
+function validaCampoB(campoA, campoB) {
+    const CampoBMaiorQueCampoA = campoB > campoA;
+    return CampoBMaiorQueCampoA;
 }
+
+campoB.addEventListener('input', function(event) {
+    console.log(event.target.value);
+    validForm = validaCampoB(campoA.value, event.target.value);
+
+    if (!validForm) {
+        campoB.classList.add('invalid');
+        document.querySelector('.invalid-message').style.display = 'block';
+        document.querySelector('.valid-message').style.display = 'none';
+        campoB.classList.remove('valid');
+        document.querySelector('.success-message').style.display = 'none';
+    } else {
+        campoB.classList.add('valid');
+        document.querySelector('.valid-message').style.display = 'block';
+        document.querySelector('.invalid-message').style.display = 'none';
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
-    const numeroContaBeneficiario = document.getElementById('numero-conta');
-    const valorDeposito = document.getElementById('valor-deposito');
-    const descricao = document.getElementById('descricao');
-    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> na conta - <b>${numeroContaBeneficiario.value}</b>`;
+    const mensagemSucesso = `Número do campo B: <b>${campoB.value}</b> é maior que o número do campo A: <b>${campoA.value}`;
+    const mensagemErro = `Número do campo B: <b>${campoB.value}</b> <b>NÃO</b> é maior que o número do campo A: <b>${campoA.value}`;
 
-    formValido = validaNome(nomeBeneficiario.value)
-    if (formValido) {
+    validForm = validaCampoB(campoA.value, campoB.value)
+    
+    if (validForm) {
         const containerMessagemSucesso = document.querySelector('.success-message');
         containerMessagemSucesso.innerHTML = mensagemSucesso;
         containerMessagemSucesso.style.display = 'block';
 
-        nomeBeneficiario.value = '';
-        numeroContaBeneficiario.value = '';
-        valorDeposito.value = '';
-        descricao.value = '';
+        campoA.value = '';
+        campoB.value = '';
 
-    } else {
-        nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
-    }
-});
-
-nomeBeneficiario.addEventListener('keyup', function(event) {
-    console.log(event.target.value);
-    formValido = validaNome(event.target.value);
-
-    if (!formValido) {
-        nomeBeneficiario.classList.add('error');
-        document.querySelector('.error-message').style.display = 'block';
-    } else {
-        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.valid-message').style.display = 'none';
+        document.querySelector('.invalid-message').style.display = 'none';
+        campoB.classList.remove('invalid');
+        campoB.classList.remove('valid');
         document.querySelector('.error-message').style.display = 'none';
+
+    } else {
+        const containerMessagemErro = document.querySelector('.error-message');
+        containerMessagemErro.innerHTML = mensagemErro;
+        containerMessagemErro.style.display = 'block';
+
+        document.querySelector('.error-message').style.display = 'block';
+        document.querySelector('.success-message').style.display = 'none';
+        document.querySelector('.valid-message').style.display = 'none';
+        campoB.classList.remove('valid');
     }
 });
